3d5150f393d934c8a9266c343224538192465a98
“te <l>
2019-12-16 17:02:35 +0800

    12-16

---
+++ src/components/power/roles.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片区域 -->
+    <el-card>
+      <!-- 添加按钮 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
